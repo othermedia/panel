cb782cf8435a8e29c5300c82e7582babe4f5c0c0
Benedict Eastaugh <benedict@eastaugh.net>
2009-04-28 00:48:23 +0100

    Take advantage of improved background position handling in Stylish.

--- panel.css.rb
+++ panel.css.rb
@@ -13,17 +13,18 @@ panel = Stylish.generate do
       :background => {:transparent => true,
                       :image => :panel_bg,
                       :repeat => "no-repeat",
-                      :position => "top right",
+                      :position => ["100%", "0"],
                       :compressed => true}
     rule ".panel-c", :position => "relative", :zoom => 1, :padding => "10px"
     rule ".panel-t", :position => "absolute", :left => 0, :top => 0, :width => "2px",
-      :margin_left => "-2px", :height => "100%", :background => {:position => "top left"}
+      :margin_left => "-2px", :height => "100%",
+      :background => {:position => ["top", "left"]}
     rule ".panel-b", :position => "relative", :width => "100%", :text_align => "left",
                      :display => "block", :height => "2px", :font_size => "1px",
-                     :background => {:position => "bottom right"} do
+                     :background => {:position => ["100%", "100%"]} do
       # Could refactor with a 'this' method
       div :display => "block", :height => "2px", :font_size => "1px", :position => "relative",
-        :width => "2px", :margin_left => "-2px", :background => {:position => "bottom left"}
+        :width => "2px", :margin_left => "-2px", :background => {:position => ["0", "100%"]}
     end
   end
 end
