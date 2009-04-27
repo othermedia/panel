8eca979936bfcbde3b4997f755fd805fb85113af
Benedict Eastaugh <benedict@eastaugh.net>
2009-04-27 11:51:56 +0100

    Panels are rich, cross-browser content wrappers.

--- README.markdown
+++ README.markdown
@@ -1,40 +1,22 @@
-Example JavaScript project
-==========================
+Panel
+=====
 
-This project contains just enough boilerplate to support creation of a new JavaScript
-project to be shared across theOTHERmedia sites. Aside from this README file, you
-should see files called widget.js and jake.yml.
+Panels are content wrappers which allow one to create rich, block-style
+interfaces that are widely interoperable with older browsers, including
+Internet Explorer 6.
 
-*widget.js* should be renamed to something appropriate for your project, and you
-should replace the word 'widget' with this new name in jake.yml (jake.yml is a build
-config file used when automatically minifying your code for deployment). Remove all
-the code in widget.js and start over with your code.
+Two JavaScript classes are included: `Panel` and `PanelOverlay`. The former
+allows for the creation of a generic panel; the latter, for an overlay which
+wraps its content in a panel.
 
-You should also replace the contents of this file with setup information for your
-project. For example, the config for this example would be:
 
-    pkg('Widget', 'http://cdn.othermedia.com/libs/widget/master/widget-min.js')
+Setup information
+-----------------
+
+    pkg('Panel', 'http://cdn.othermedia.com/libs/panel/master/panel-min.js')
         .requires('JS.Class')
         .requires('Ojay')
         .requires('Ojay.Observable')
-        .requires('Ojay.HTTP')
-        .requires('Ojay.Paginator');
-
-See [the example JavaScript config][1] for more information.
-
-[1]: http://github.com/othermedia/js-packages
-
-
-Creating a new project
-======================
-
-    git clone git@github.com:othermedia/example-project.git my-widget
-    cd my-widget
-    git remote rm origin
-    git remote add origin git@github.com:othermedia/my-widget.git
-    
-    (edit)
-    
-    git commit -am "Commit message"
-    git push origin master
-
+        .requires('Ojay.HTML')
+        .requires('Ojay.Overlay')
+        .requires('Ojay.ContentOverlay');
---
+++ panel.js
@@ -0,0 +1,92 @@
+/**
+ * @constructor
+ * @class Panel
+ */
+var Panel = new JS.Class({
+    /**
+     * @param {String} classes
+     * @param {Function|String|HTMLElement} block
+     * @param {HtmlBuilder} HTML
+     */
+    initialize: function(classes, block, HTML) {
+        this._classes = classes;
+        this._block = block;
+        this._HTML = HTML;
+        this.getHTML();
+    },
+    
+    /**
+     * @returns {HTMLDivElement}
+     */
+    getHTML: function() {
+        if (this._html) return this._html;
+        var self = this, classes = self._classes, block = self._block, HTML = self._HTML;
+        var builder = (typeof block == 'function');
+        HTML = HTML || Ojay.HTML;
+        this._html = Ojay( HTML.div({className: 'panel' + (classes ? ' ' + classes : '')}, function(HTML) {
+            HTML.div({className: 'panel-c'}, function(HTML) {
+                HTML.div({className: 'panel-t'});
+                self._content = Ojay( HTML.div(function(HTML) {
+                   if (builder) block(HTML);
+                }) );
+            });
+            if (!builder) self._content.insert(block, 'bottom');
+            HTML.div({className: 'panel-b'}, function(HTML) {
+                HTML.div();
+            });
+        }) );
+        return this.getHTML();
+    },
+    
+    /**
+     * @returns {Panel}
+     */
+    setContent: function() {
+       this._content.setContent.apply(this._content, arguments);
+       return this;
+    },
+    
+    /**
+     * @returns {Panel}
+     */
+    insert: function() {
+       this._content.insert.apply(this._content, arguments);
+       return this;
+    }
+});
+
+/**
+ * @constructor
+ * @class PanelOverlay
+ */
+var PanelOverlay = new JS.Class(Ojay.ContentOverlay, {
+    /**
+     * @param {Object} options
+     */
+    initialize: function(options) {
+        this._panel = new Panel();
+        var closeButton = Ojay( Ojay.HTML.div({className: 'close-button'}, 'Close') );
+        
+        this.callSuper();
+        
+        this.getContainer().insert(closeButton, 'top');
+        closeButton.on('click')._(this).hide('fade');
+        this.getContentElement().insert(this._panel.getHTML());
+    },
+    
+    /**
+     * @returns {PanelOverlay}
+     */
+    setContent: function() {
+        this._panel.setContent.apply(this._panel, arguments);
+        return this;
+    },
+    
+    /**
+     * @returns {PanelOverlay}
+     */
+    insert: function() {
+        this._panel.insert.apply(this._panel, arguments);
+        return this;
+    }
+});
